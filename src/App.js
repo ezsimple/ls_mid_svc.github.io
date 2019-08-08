4a161aa426e86d03e513f64954a613b645bfbf54
mhlee <mhlee@ezfarm.co.kr>
2019-08-08 14:57:32 +0900

    .

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import About from './About';
 import Home from './Home';
 import NotFound from './NotFound';
+import Req from 'containers/';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
+            <Route exact path="/req" component={Req} />
             <Route path="" componet={NotFound} />
           </Switch>
         </div>
